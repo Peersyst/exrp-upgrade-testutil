164cf103d6d2085cc1a036dfb932e44d1d22c125
AdriaCarrera <adria.cm95@gmail.com>
2024-05-09 17:45:58 +0200

    init

--- 01_update_genesis.sh
+++ 01_update_genesis.sh
@@ -10,7 +10,7 @@ COSMOVISOR_IMAGE=cosmovisor:local
 docker run $CUSTOM_DOCKER_FLAGS -it --rm --entrypoint "" -v $DEFAULT_NODE_HOME:/root/.exrpd peersyst/xrp-evm-blockchain:latest exrpd tendermint unsafe-reset-all
 rm -rf $DEFAULT_NODE_HOME/cosmovisor
 
-node src/build-genesis.js genesis-state-devnet.json $DEFAULT_NODE_HOME/config/genesis.json
+node src/build-genesis.js genesis-state.json $DEFAULT_NODE_HOME/config/genesis.json
 
 docker run $CUSTOM_DOCKER_FLAGS -it --rm --name validator -p 26657:26657 -p 8545:8545 -v $DEFAULT_NODE_HOME:/root/.exrpd $COSMOVISOR_IMAGE initialize
 docker run $CUSTOM_DOCKER_FLAGS -it --rm --name validator -p 26657:26657 -p 8545:8545 -v $DEFAULT_NODE_HOME:/root/.exrpd $COSMOVISOR_IMAGE cosmovisor run start
