a2bce5b8472de34fe73560b4c00d517e485ba576
AdriaCarrera <adria.cm95@gmail.com>
2024-05-28 12:59:13 +0200

    init

--- 02_vote_upgrade.sh
+++ 02_vote_upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEFAULT_NODE_HOME=$PWD/data/node-0
-PROPOSAL_ID=33
+PROPOSAL_ID=36
 CONTAINER_NAME=validator
 
 CUSTOM_DOCKER_FLAGS="--platform linux/amd64"
@@ -14,7 +14,7 @@ export -f exrpd
 # Copy the upgrade-proposal.json file inside the container
 docker cp data/upgrade-proposal.json $CONTAINER_NAME:/root/upgrade-proposal.json
 
-exrpd tx gov submit-proposal /root/upgrade-proposal.json --from alice --gas-prices 800000000000axrp --gas 400000 --yes --gas 300000
+exrpd tx gov submit-proposal /root/upgrade-proposal.json --from alice --gas-prices 8000000000000axrp --gas 400000 --yes
 sleep 10
 exrpd tx gov vote $PROPOSAL_ID yes --from alice --gas-prices 800000000000axrp --yes
 sleep 60
