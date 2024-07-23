fae6ae5678b4ec8d004845949aeee79db457242d
AdriaCarrera <adria.cm95@gmail.com>
2024-07-23 16:04:14 +0200

    change binary

--- 00_download_state.sh
+++ 00_download_state.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -v /root:/root --rm -it --entrypoint "" peersyst/xrp-evm-blockchain:latest sh -c "exrpd export > /root/.exrpd/exported-state.json"
+docker run -v /root:/root --rm -it --entrypoint "" peersyst/exrp:latest sh -c "exrpd export > /root/.exrpd/exported-state.json"
 
 mv /root/.exrpd/exported-state.json ./data/genesis-state.json
