27f896f477cde5a042c8aa607a5022f653fd513f
AdriaCarrera <adria.cm95@gmail.com>
2024-07-23 16:23:23 +0200

    add validator proposal

--- 02_vote_upgrade.sh
+++ 02_vote_upgrade.sh
@@ -12,9 +12,11 @@ exrpd() {
 export -f exrpd
 
 # Copy the upgrade-proposal.json file inside the container
-docker cp data/upgrade-proposal.json $CONTAINER_NAME:/root/upgrade-proposal.json
+# docker cp data/upgrade-proposal.json $CONTAINER_NAME:/root/upgrade-proposal.json
+#
+# exrpd tx gov submit-proposal /root/upgrade-proposal.json --from alice --gas-prices 8000000000000axrp --gas 400000 --yes
 
-exrpd tx gov submit-proposal /root/upgrade-proposal.json --from alice --gas-prices 8000000000000axrp --gas 400000 --yes
+exrpd tx poa add-validator --address ethm1gzsalqy0rru2pe6az9ulk69hg2dnm22wrw3sl7 --moniker flowdesk --pubkey '{"@type":"/cosmos.crypto.ed25519.PubKey","key":"ed0xYtgWL/bLw7xe8ldSUW/NIU2ZgSWFhfKsUfpT1gc="}' --title 'Add Flowdesk validator' --summary 'Add Flowdesk validator' --from alice --gas-prices 8000000000000axrp --gas 400000 --yes
 sleep 10
 exrpd tx gov vote $PROPOSAL_ID yes --from alice --gas-prices 800000000000axrp --yes
 sleep 60
